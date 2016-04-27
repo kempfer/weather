b5d9a70ee3b07a9b97ce1b542044a86436f07bc6
Зотов Максим < >
2016-04-28 00:42:42 +0300

    add functional geo-coordinates determination

--- app/src/main/java/com/example/zotov/weather/MainActivity.java
+++ app/src/main/java/com/example/zotov/weather/MainActivity.java
@@ -1,11 +1,40 @@
 package com.example.zotov.weather;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    LocationManager locationManager;
+
+    TextView textLocation;
+
+    private static final String[] INITIAL_PERMS={
+            Manifest.permission.ACCESS_FINE_LOCATION
+    };
+
+    private static final String[] LOCATION_PERMS = {
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION
+    };
+
+    private static final int INITIAL_REQUEST = 1337;
+
+    private static final int LOCATION_REQUEST = INITIAL_REQUEST + 1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +44,110 @@ public class MainActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+
+
+        this.getSystemService(LOCATION_SERVICE);
+
+        textLocation = (TextView) findViewById(R.id.location);
+
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+        if (!canAccessLocation()) {
+            requestPermissions(INITIAL_PERMS, INITIAL_REQUEST);
+        }
+
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if(canAccessLocation()) {
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+                    1000 * 10, 10, locationListener);
+            locationManager.requestLocationUpdates(
+                    LocationManager.NETWORK_PROVIDER, 1000 * 10, 10,
+                    locationListener);
+        }
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if(canAccessLocation()) {
+            locationManager.removeUpdates(locationListener);
+        }
+
+    }
+
+    private void showLocation(Location location) {
+        textLocation.setText(formatLocation(location));
+        
+        locationManager.removeUpdates(locationListener);
+    }
+
+    private String formatLocation(Location location) {
+        if (location == null)
+            return "";
+        return String.format(
+                "Coordinates: lat = %1$.4f, lon = %2$.4f",
+                location.getLatitude(), location.getLongitude());
+    }
+
+
+    private LocationListener locationListener = new LocationListener() {
+
+        @Override
+        public void onLocationChanged(Location location) {
+            showLocation(location);
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+            System.out.println("onProviderDisabled");
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+            showLocation(locationManager.getLastKnownLocation(provider));
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
+    };
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch(requestCode) {
+            case LOCATION_REQUEST:
+                if (canAccessLocation()) {
+                    doLocationThing();
+                }
+                else {
+                    bzzzt();
+                }
+                break;
+        }
+    }
+
+    private void bzzzt() {
+        Toast.makeText(this, "Bzzzz", Toast.LENGTH_LONG).show();
+    }
+
+
+    private boolean canAccessLocation() {
+        return(hasPermission(Manifest.permission.ACCESS_FINE_LOCATION));
+    }
+
+    private boolean hasPermission(String perm) {
+        return(PackageManager.PERMISSION_GRANTED==checkSelfPermission(perm));
+    }
+
+    private void doLocationThing() {
+        Toast.makeText(this, "Location", Toast.LENGTH_SHORT).show();
     }
 }
